a140f71ee76a9ee6f9717859273f1b9920f8891b
Matheus De Nigris <matheusdenigris@hotmail.com>
2023-08-03 13:39:52 -0300

    feat: estilizando a HomePage e a HistoryPage

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
-import { History } from "./pages/History";
+import { History } from "./pages/History/Index";
 import { DefaultLayout } from "./layouts/DefaultLayout/index";
 
 export function Router() {
